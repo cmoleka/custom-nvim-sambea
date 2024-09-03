7e662c34acc78d40a886c252947dfa5aa0c2d4c0
Sambea <sambea@Sambeas-MacBook-Pro.local>
2024-09-02 22:13:23 -0400

    initial commit, worknig config

---
+++ init.lua
@@ -0,0 +1,72 @@
+vim.opt.showmode = true
+vim.opt.relativenumber = true
+vim.opt.hlsearch = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.list = false
+vim.opt.inccommand = "split"
+vim.opt.scrolloff = 0
+vim.opt.smartindent = false
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
+vim.opt.shiftround = true
+-- vim.opt.guifont = "FiraCode Nerd Font:h12"
+vim.opt.cmdheight = 1
+vim.opt.updatetime = 50
+vim.g.have_nerd_font = true
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- add your own keymapping
+require("keybindings")
+-- Lazy.nvim package manager
+require("config.lazy")
+-- Add automations
+require("automation")
