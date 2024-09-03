65debb21d09a9d89a5c97b85717071bfd2a27e8e
Sambea <me@carlomoleka.com>
2024-09-03 13:46:20 -0400

    completed basic setup, lsp, lint, formating, plugins

---
+++ lua/core/keybindings.lua
@@ -0,0 +1,56 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set keymap to local variable
+local keymap = vim.keymap
+
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'kj', '<ESC>', { noremap = true, silent = true })
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- window management
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' }) -- split window vertically
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' }) -- split window horizontally
+keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' }) -- make split windows equal width & height
+keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' }) -- close current split window
+
+keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
+keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
+keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
+keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
+keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
+
+-- Remap enter to make a new line below curson (macOS) in normal mode
+vim.api.nvim_set_keymap('n', '<CR>', 'o<ESC>', { noremap = true, silent = true })
+-- Remap shift enter to make a new line above cursor (macOS) in normal mode
+vim.api.nvim_set_keymap('n', '<S-CR>', 'O<ESC>', { noremap = true, silent = true })
+
+-- Navigate to next buffer
+vim.api.nvim_set_keymap('n', '<S-l>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-h>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,67 +1,15 @@
-if vim.fn.has("nvim-0.9.0") == 0 then
+if vim.fn.has 'nvim-0.9.0' == 0 then
   vim.api.nvim_echo({
-    { "LazyVim requires Neovim >= 0.9.0\n", "ErrorMsg" },
-    { "Press any key to exit", "MoreMsg" },
+    { 'LazyVim requires Neovim >= 0.9.0\n', 'ErrorMsg' },
+    { 'Press any key to exit', 'MoreMsg' },
   }, true, {})
   vim.fn.getchar()
-  vim.cmd([[quit]])
+  vim.cmd [[quit]]
   return {}
 end
 
-
 return {
-"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-"tpope/vim-surround",
-{
-    "williamboman/mason.nvim"
-},
-{
-  "MeanderingProgrammer/render-markdown.nvim", --render-markdown.nvim is an optional dependency that is used to render the markdown content of the chat history.
-  opts = {
-    file_types = { "markdown", "Avante" },
-  },
-  ft = { "markdown", "Avante" },
-},
-{
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  lazy = false,
-  opts = {
-    -- add any opts here
-  },
-  build = ":AvanteBuild", -- This is optional, recommended tho. Also note that this will block the startup for a bit since we are compiling bindings in Rust.
-  dependencies = {
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to setup it properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
-}
+  'nvim-lua/plenary.nvim', -- lua functions that many plugin use
+  'christoomey/vim-tmux-navigator', -- tmux & split window navigation
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 }
